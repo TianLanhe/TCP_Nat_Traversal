d179f094d3d9218e6c89bfc03e9178b28deaaed6
hezhenyu <849416763@qq.com>
2018-03-20 10:47:14 +0800

    NAT类型测试

--- main/client.cpp
+++ main/client.cpp
@@ -1,6 +1,6 @@
 #include "../include/transmission/TransmissionData.h"
 #include "../include/transmission/TransmissionProxy.h"
-#include "../include/socket/DefaultSocketFactory.h"
+#include "../include/socket/ReuseSocketFactory.h"
 #include "../include/socket/ClientSocket.h"
 
 #include <iostream>
@@ -9,35 +9,100 @@
 using namespace std;
 using namespace Lib;
 
+ClientSocket::port_type stringToShort(const string& str){
+    ClientSocket::port_type port = 0;
+    for(string::size_type i = 0;i<str.size();++i){
+        port = port * 10 + str[i] - '0';
+    }
+    return port;
+}
+
+TransmissionData getIpAndPort(const string& clientIp,ClientSocket::port_type clientPort,const string& serverIp,ClientSocket::port_type serverPort){
+    TransmissionData ret;
+
+    ClientSocket *client = ReuseSocketFactory::GetInstance()->GetReuseClientSocket();
+    if(!client->bind(clientPort,clientIp)){
+        cout << "client bind " << clientIp << ":" << clientPort << " error!" << endl;
+        return ret;
+    }
+
+    if(!client->connect(serverIp,serverPort)){
+        cout << "client connect to " << serverIp << ":" << serverPort << " error!" << endl;
+        return ret;
+    }
+
+    TransmissionProxy proxy(client);
+
+    ret = proxy.read();
+
+    return ret;
+}
+
+void output(const string& ip,ClientSocket::port_type port){
+    cout << "#####################" << endl;
+    cout << "Client's IP and Port:" << endl;
+    cout << "IP: " << ip << endl;
+    cout << "Port: " << port << endl;
+    cout << "#####################" << endl;
+}
+
 int main(int argc,char *argv[]){
 	if(argc != 3){
-		cout << "Usage: client serverIp serverPort" << endl;
+        cout << "Usage: client clientIp clientPort" << endl;
 		return 1;
 	}
 
-	string ip = argv[1];
-	string strPort = argv[2];
-	
-	ClientSocket::port_type port = 0;
-	for(string::size_type i = 0;i<strPort.size();++i){
-		port = port * 10 + strPort[i] - '0';
-	}	
+    string clientIp = argv[1];
+    string strPort = argv[2];
+    ClientSocket::port_type clientPort = stringToShort(strPort);
 
-	ClientSocket *client = DefaultSocketFactory::GetInstance()->GetDefaultClientSocket();
+    string serverIp;
+    ClientSocket::port_type serverPort = 0;
 
-	if(!client->connect(ip,port)){
-		cout << "connect to " << ip << " " << port << " error!" << endl;
-		return 1;	
-	}
+    cout << "Please input the first ip:port of server." << endl;
+    cout << "IP: ";
+    cin >> serverIp;
+    cout << "Port: ";
+    cin >> serverPort;
 
-	TransmissionProxy proxy(client);
+    output(clientIp,clientPort);
 
-	TransmissionData data = proxy.read();
+    TransmissionData data = getIpAndPort(clientIp,clientPort,serverIp,serverPort);
+    output(data.getString("ip"),data.getInt("port"));
 
-	cout << "ip: " << client->getAddr() << endl;
-	cout << "port: " << client->getPort() << endl;
-	cout << "peer_ip: " << data["ip"] << endl;
-	cout << "peer_port: " << data["port"] << endl;
+    if(clientIp == data.getString("ip") && clientPort == data.getInt("port")){
+        cout << "do not have NAT." << endl;
+        return 0;
+    }
+
+    cout << "Please input the second ip:port of server." << endl;
+    cout << "IP: ";
+    cin >> serverIp;
+    cout << "Port: ";
+    cin >> serverPort;
+
+    TransmissionData data2 = getIpAndPort(clientIp,clientPort,serverIp,serverPort);
+    output(data2.getString("ip"),data2.getInt("port"));
+
+    if(data2.getString("ip") == data.getString("ip") && data2.getInt("port") == data.getInt("port")){
+        cout << "Endpoint independent." << endl;
+        return 0;
+    }
+
+    cout << "Please input the third ip:port of server." << endl;
+    cout << "IP: ";
+    cin >> serverIp;
+    cout << "Port: ";
+    cin >> serverPort;
+
+    TransmissionData data3 = getIpAndPort(clientIp,clientPort,serverIp,serverPort);
+    output(data3.getString("ip"),data3.getInt("port"));
+
+    if(data2.getString("ip") == data3.getString("ip") && data2.getInt("port") == data3.getInt("port")){
+        cout << "Address dependent." << endl;
+    }else{
+        cout << "Address and Port dependent." << endl;
+    }
 
 	return 0;
 }
